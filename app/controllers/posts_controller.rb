6725565afec2d1ff9cecde7ca1dfa508f225aa18
lucascolusso <lucascolusso@gmail.com>
2014-12-24 14:50:39 -0800

    voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -37,6 +38,11 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to root_path
+  end
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_to :back
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
+    member do
+      get "like", to: "posts#upvote"
+    end
     resources :comments
   end
 
